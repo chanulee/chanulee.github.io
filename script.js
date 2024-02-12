7b8d20f6f4e9b458b362fdf25e8500761dc23666
Chanu Lee <70347032+chanulee@users.noreply.github.com>
2024-02-12 17:37:27 +0000

    js update

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.querySelectorAll('.list-item').forEach(item => {
-	item.addEventListener('click', () => {
-		const h5 = item.querySelector('.list-info h5');
-		h5.classList.toggle('show');
+	item.addEventListener('click', event => {
+		// Check if the screen width is more than 620px
+		if (window.innerWidth > 620) {
+			// If it is, don't perform the toggle action
+			return;
+		}
+		// Otherwise, toggle the 'show' class on the h5 element
+		const h4 = event.currentTarget.querySelector('.list-info h4');
+		h4.classList.toggle('show');
 	});
 });
